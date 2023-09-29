875a45e98d7589777b9b65f773b206123a650a78
Blake Ferkingstad <bferkingstad@gmail.com>
2023-09-28 20:52:12 -0500

    Create API service that has docker abilities

---
+++ README.md
@@ -0,0 +1,11 @@
+# Description
+Currently just a project where I am learning more about containers
+
+
+## Project roadmap/goals
+
+- Create *API* to return dynamic ish data
+- Create *Frontend* that talks to our API
+- Create *Dockerfiles* for API and FE
+- Have them talk to each other and be able to spin them up with docker or kubernetes
+
---
+++ goAPI/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type currentTimeModel struct {
+	Language string    `json:"language"`
+	CT       time.Time `json:"ct"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/currentTime", getCurrentTime)
+
+	router.Run("0.0.0.0:8080")
+}
+
+func getCurrentTime(c *gin.Context) {
+	var result = currentTimeModel{
+		Language: "GO",
+		CT:       time.Now(),
+	}
+
+	c.IndentedJSON(http.StatusOK, result)
+}
