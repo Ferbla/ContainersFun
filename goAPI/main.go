7be3e31225d4f4c5101b639ee77ab4a8205b47a1
Blake Ferkingstad <bferkingstad@gmail.com>
2024-05-24 21:02:52 -0500

    Add CORS and version property to currentTime endpoint

--- goAPI/main.go
+++ goAPI/main.go
@@ -4,16 +4,20 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 type currentTimeModel struct {
 	Language string    `json:"language"`
 	CT       time.Time `json:"ct"`
+	Version  string    `json:"version"`
 }
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
+
 	router.GET("/currentTime", getCurrentTime)
 
 	router.Run("0.0.0.0:3000")
@@ -23,6 +27,7 @@ func getCurrentTime(c *gin.Context) {
 	var result = currentTimeModel{
 		Language: "GO",
 		CT:       time.Now(),
+		Version:  "0.0.2",
 	}
 
 	c.IndentedJSON(http.StatusOK, result)
